ff038891d2ccf2431644f6be7d6e3042af35f5ed
Viviane Mori <vivianeendomori@gmail.com>
2025-04-03 17:41:55 -0300

    feat: add tela inicial cliente e rotas

--- src/main/client/src/main.tsx
+++ src/main/client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, Routes, Route} from "react-router";
+import LoginComponent from "./pages/login/LoginComponent.tsx";
+import ClientHome from "./pages/ClientHome";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<ClientHome />}/>
+              <Route path="login"  element={<LoginComponent />} />
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/main/client/src/pages/ClientHome/index.tsx
@@ -0,0 +1,48 @@
+import {ChevronDownIcon} from "@heroicons/react/16/solid";
+import styles from "./styles.module.scss";
+
+export default function ClientHome () {
+    return (
+        <>
+            <div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="origin" className="block text-sm/6 font-medium text-gray-900">
+                        Origem
+                    </label>
+                    <div className="mt-2">
+                        <div
+                            className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                            <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"/>
+                            <input
+                                id="origin"
+                                name="origin"
+                                type="text"
+                                placeholder="Curitiba (CWB)"
+                                className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="origin" className="block text-sm/6 font-medium text-gray-900">
+                        Destino
+                    </label>
+                    <div className="mt-2">
+                        <div
+                            className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                            <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"/>
+                            <input
+                                id="destination"
+                                name="destination"
+                                type="text"
+                                placeholder="São Paulo (GRU)"
+                                className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
