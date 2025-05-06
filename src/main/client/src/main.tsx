d4077d7665e7d7c9b6d530b50e5f7a4f12923531
Viviane Mori <vivianeendomori@gmail.com>
2025-05-06 02:21:22 -0300

    fix: faz rebase mudancas

--- src/main/client/src/main.tsx
+++ src/main/client/src/main.tsx
@@ -7,16 +7,4 @@ createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
 	</StrictMode>
-)
-
-                <Routes>
-                    <Route path="/" element={<ClientHome />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="cadastro" element={<CadastroComponent />} />
-                    <Route path="search" element={<SearchResults />} />
-                    <Route path="milhas/comprar" element={<ComprarMilhasComponent />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    </StrictMode>
 );
